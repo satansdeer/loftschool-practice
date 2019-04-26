5324fb927821cb893f29b25bcf142e8da8559114
Maksim Ivanov <satansdeer@gmail.com>
2019-04-26 17:34:33 +0200

    Add lazy examples

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ContextExample from './ContextExample';
-import RefExample from './RefExample';
-import ForwardRefExample from './ForwardRefExample';
-import FormInputsValidation from './FormInputsValidation';
-import HooksExample from './HooksExample'
-import RenderProps from './RenderProps'
-import GenerateLayout from './GenerateLayout';
-import ReactRouterExample from './ReactRouterExample';
+import ReactLoadableExample from './ReactLoadableExample';
+import ReactLazyExample from './ReactLazyExample';
+import ReduxDevToolsExample from './ReduxDevToolsExample'
+import ReactMobxExample from './ReactMobxExample'
+import ReactMemoizationExample from './ReactMemoizationExample';
+import ContextHooksExample from './ContextHooksExample';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ContextExample />, document.getElementById('root'));
+ReactDOM.render(<ReactLazyExample />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
