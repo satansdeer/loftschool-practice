07649bf8c1f27a62386dee88f1e7d1fbe39cff6c
Maksim Ivanov <satansdeer@gmail.com>
2019-05-05 20:27:10 +0200

    Add tests block

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import './ReduxThunkExample'
-import './ReduxSagaExample'
+// import './ReduxSagaExample'
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<R />, document.getElementById('root'));
