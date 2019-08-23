7d123fadcd329ecc6dd315bccafc60fc5703ba1e
Maksim Ivanov <satansdeer@gmail.com>
2019-08-23 20:38:32 +0200

    Update index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Component from './ReactLoadableExample';
 
-// ReactDOM.render(<Component>, document.getElementById("root"));
+ReactDOM.render(<Component/>, document.getElementById("root"));
